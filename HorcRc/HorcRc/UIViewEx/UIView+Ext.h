19300b7ff31c1ed95074efba01ddcf4a164a7cbf
rck <wstckang@163.com>
2017-02-18 13:58:26 +0800

    first commit

---
+++ HorcRc/HorcRc/UIViewEx/UIView+Ext.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Ext.h
+//  HorcRc
+//
+//  Created by john on 2017/2/18.
+//  Copyright © 2017年 john. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Ext)
+
+@end
---
+++ HorcRc/HorcRc/UIViewEx/UIView+Ext.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Ext.m
+//  HorcRc
+//
+//  Created by john on 2017/2/18.
+//  Copyright © 2017年 john. All rights reserved.
+//
+
+#import "UIView+Ext.h"
+
+@implementation UIView (Ext)
+
+@end
